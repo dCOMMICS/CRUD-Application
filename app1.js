0b521153759412112375eea0206c506b2541cab1
Craig Wurld <85875550+dCOMMICS@users.noreply.github.com>
2023-05-28 11:02:42 +0300

    saas

--- app1.js
+++ app1.js
@@ -1,1 +1,15 @@
 console.console.log(); 
+
+
+
+let firstName = 'Minnesota ';
+
+let dateofBirth = 1993;
+
+let currentYear = 2014;
+
+CurrentAge = (currentYear - dateofBirth);
+
+//.console.log(); 
+
+console.log(CurrentAge);
